796bf6476784e405d4ccf525984cfb5a14be8a1e
AEROLIE <1336462055@qq.com>
2017-07-06 18:10:32 +0800

    js dom

---
+++ js-display.js
@@ -0,0 +1,9 @@
+function hidetext(){
+	var a=document.getElementById('con');
+	a.style.display='none';
+}// JavaScript Document
+function showtext(){
+	var b=document.getElementById('con');
+	b.style.display='block';
+	
+}
