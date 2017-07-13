b58f5c91161a485f2c9de5fafd908ec8b9d3c853
AEROLIE <1336462055@qq.com>
2017-07-13 18:14:17 +0800

    js event

--- js-event.js
+++ js-event.js
@@ -1,3 +1,11 @@
 // JavaScript Document
 function openwin(){	window.open('http://www.imooc.com','_blank','height=600,width=400,top=100,toolbar=no,left=0,menu=no,scrollbars=no,status=no');
 }
+
+function info(){
+	confirm('请输入姓名后，再点击确认');
+}
+
+function message(){
+	alert('不要移开，点击后进行慕课网！');
+}
