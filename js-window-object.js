eaa6ca39b2a5b6fd05340f92d2852bf485929a07
AEROLIE <1336462055@qq.com>
2017-08-11 17:56:30 +0800

    js window object 2

--- js-window-object.js
+++ js-window-object.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var attime;
+function clock()
+{
+	var time = new Date();
+	attime = time.getHours()+':'+time.getMinutes()+':'+time.getSeconds();
+	document.getElementById('clock').value=attime;
+}
+var int = setInterval('clock()',1000);
